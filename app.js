3eb6542b011f3259318ea017616d88b4cb79103b
As <andrewshield99@hotmail.com>
2013-10-03 21:49:07 +1300

    Second

--- app.js
+++ app.js
@@ -1,30 +1,72 @@
 // Generate random number onload
-var randomNumber = Math.floor(Math.random()*101);
-	console.log (randomNumber);
+var randomNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+console.log (randomNumber);
 
-// Prompt user to guess number
-var guess = function () {
-	var userGuess = prompt ("Guess a number between 1 - 100");
-	console.log (userGuess);
+// Submitting user input
+$(document).ready(function (){
+  $('#submit').click(submit);
+    $('#guess').keydown(function(e){
+      if (e.keyCode == 13) {
+        submit();
+      };
+    });
+}); 
+
+// Storing user input
+  function submit(){
+    var guess = $('#guess').val();
+    console.log(guess);
+    validation(guess);
+
+// Reset form   
+    $( "#guess" ).val('')
+  }
+  
+// Validating user input
+var shake = function(){
+    $("#guess").addClass('shake animated').delay(1000).queue(function(next){
+    $(this).removeClass('shake animated');
+    next();
+    });
+  }
+
+  
+  var validation = function (guess) {
+    if (this == "" || isNaN(this))
+    shake()
+
+    else if (this > 100 || this <= 0) {
+    shake()
+
+  }
 
 // Compare number guessed back to number generated
-if (randomNumber == userGuess) {
-	console.log ("Correct!");
-}	
+  if (randomNumber == guess) {
+	alert("Correct!");
+  randomNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+  console.log (randomNumber);
+  }
+  else if (randomNumber < guess) {
+	 console.log("Guess lower");
+  }
 
-else if (randomNumber < userGuess) {
-	console.log ("Guess lower");
-	guess ();
-
+  else if (randomNumber > guess) {
+	console.log("Guess higher");
+  }
 }
 
-else if (randomNumber > userGuess) {
-	console.log ("Guess higher");
-	guess();
-}
 
-};
 
-guess();
+
+
+
+
+
+
+
+
+
+
+
 
 
